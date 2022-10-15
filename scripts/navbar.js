030b61344335a3541809934a615f513f64b3d4b0
Christio02 <112630243+Christio02@users.noreply.github.com>
2022-10-15 17:51:21 +0200

    Added test template for navbar, finished navbar.js

---
+++ scripts/navbar.js
@@ -0,0 +1,27 @@
+var navbar = `
+ <nav class = "navbar">
+<div class = "logo" >
+    <a href="test.html"> 
+        <img src="/kada-kaba-logo-zip-file/png/logo-color.png" alt="logo-color">
+    </a>
+</div>
+
+<ul class = "main-nav">
+
+<li>
+    <a href="/Final-Website/page/about-us/about-us.html" class = "nav-links">About us</a>
+</li>
+
+<li>
+    <a href="/Final-Website/page/our-work/our-work.html" class = "nav-links">Our Work</a>
+</li>
+
+<li>
+    <a href="/Final-Website/page/forest/amazon.html" class = "nav-links">Amazon</a>
+</li>
+</ul>
+</nav>
+`;
+
+document.body.insertAdjacentHTML('afterbegin', navbar);
+

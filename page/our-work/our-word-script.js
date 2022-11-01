e0a2c8572de644c308b38ac8ad637214703cecb4
B1uskyy <mats.kvanvik@hotmail.com>
2022-11-01 08:08:09 +0100

    before hero banner commit

--- page/our-work/our-word-script.js
+++ page/our-work/our-word-script.js
@@ -1,12 +1,13 @@
 
 var i = 0; // start 
 var images = [];
-var time = 5000; // milliseconds for each image
+var time = 3000; // milliseconds for each image
 
 
 images[0] = "/image/man-in-forest.jpeg";
 images[1] = '/image/hut-in-forest.jpg'
-// images[2] = //image3.jpg
+images[2] = "/image/worker-in-forest.jpg";
+images[3] = "/image/rainforest-landscape.jpg";
 
 
 

176926f924c7980b4c0a11d3ea0b0151c0d492bb
Christio02 <112630243+Christio02@users.noreply.github.com>
2022-11-10 15:52:08 +0100

    Fixed some stuff related to footer.js, added a leaf gif, tried adding same functionality to navbar.js

--- script/navbar.js
+++ script/navbar.js
@@ -1,9 +1,13 @@
-let navbar = `
-<div class="navbar">
+// this class creates a custom "footer" element for every page on the website so that we can use modularity
+class NavBar extends HTMLElement { // source : https://stackoverflow.com/questions/69961205/insert-header-and-footer-on-all-webpages-using-javascript
+  connectedCallback() {              // https://developer.mozilla.org/en-US/docs/Web/Web_Components
+      //  refers back to the class object
+      this.innerHTML = `  
+      <div class="navbar">
       <a href="#" class="navbar-item">Home</a>
       <a href="#" class="navbar-item">About </a>
       <div class="drop">
-        <a class="drop-button">Drop</a>
+        <button class="drop-button">Drop</button>
         <div class="drop-content">
           <a href="#">A</a>
           <a href="#">A</a>
@@ -12,8 +16,7 @@ let navbar = `
       </div>
       <a id="donate" href="#">Donate</a>
     </div>
-`;
-
-document.body.insertAdjacentHTML('afterbegin', navbar);
-
-
+      `
+  }
+}
+window.customElements.define('navbar', AppHeader) // accesses the new custom element made over 

3af6ae9256e5434ff59d80f9903197973f4fcaff
B1uskyy <mats.kvanvik@hotmail.com>
2022-11-10 14:15:40 +0100

    change script image link

--- script/our-work-script.js
+++ script/our-work-script.js
@@ -4,10 +4,10 @@ var images = [];
 var time = 2500; // milliseconds for each image
 
 
-images[0] = "/image/man-in-forest.jpeg";
-images[1] = '/image/hut-in-forest.jpg'
-images[2] = "/image/worker-in-forest.jpg";
-images[3] = "/image/rainforest-landscape.jpg";
+images[0] = "../../image/man-in-forest.jpeg";
+images[1] = '../../image/hut-in-forest.jpg'
+images[2] = "../../image/worker-in-forest.jpg";
+images[3] = "../../image/rainforest-landscape.jpg";
 
 let bilde = document.getElementById("slide")
 

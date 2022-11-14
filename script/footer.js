d10d13cb0a50b9de7b844386a486756b5c23ee83
Christio02 <112630243+Christio02@users.noreply.github.com>
2022-11-14 16:41:37 +0100

    Misc

--- script/footer.js
+++ script/footer.js
@@ -3,37 +3,40 @@ class MyFooter extends HTMLElement { // source : https://stackoverflow.com/quest
     connectedCallback() {              // https://developer.mozilla.org/en-US/docs/Web/Web_Components
         //  refers back to the class object
         this.innerHTML = ` 
+        <link rel="stylesheet" href="../../css/index.css">
         <link rel="stylesheet" href="/test.css" 
-        <footer> 
-      <div class="main">
-        <div class="contact-footer">
-          <ul>
-            <li>Contact:</li>
-            <li>Loremipsum@ntnu.no</li>
-            <li>Klostergata 29 Brazil caralho</li>
-            <li>Postnr ellerno idk</li>
-          </ul>
-        </div>
+        <div class="nav-wave">
+            <footer> 
+        <div class="main">
+            <div class="contact-footer">
+            <ul>
+                <li>Contact:</li>
+                <li>Loremipsum@ntnu.no</li>
+                <li>Klostergata 29 Brazil caralho</li>
+                <li>Postnr ellerno idk</li>
+            </ul>
+            </div>
 
-        <div class="logo">
-          <img id = "leaf" src="/GIF/leaf.gif" alt="logo">
-        </div>
+            <div class="logo">
+            <img id = "leaf" src="/GIF/leaf.gif" alt="logo">
+            </div>
 
-        <div class="media">
-          <ul>
-            <li>
-              <a href="#"><img src="/image/twitter.png" alt="Twitter" /></a>
-            </li>
-            <li>
-              <a href="#"><img src="/image/youtube.png" alt="YouTube" /></a>
-            </li>
-            <li>
-              <a href="#"><img src="/image/instagram.png" alt="Instagram" /></a>
-            </li>
-          </ul>
+            <div class="media">
+            <ul>
+                <li>
+                <a href="#"><img src="/image/twitter.png" alt="Twitter" /></a>
+                </li>
+                <li>
+                <a href="#"><img src="/image/youtube.png" alt="YouTube" /></a>
+                </li>
+                <li>
+                <a href="#"><img src="/image/instagram.png" alt="Instagram" /></a>
+                </li>
+            </ul>
+            </div>
         </div>
-      </div>
-    </footer>
+        </footer>
+    </div>
         `
     }
 }

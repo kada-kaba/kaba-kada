1f76b9cda790a23019bc85c46519abc27b9886fe
leoky <leoky@stud.ntnu.no>
2022-11-15 14:36:44 +0100

    Fiksa wave på footer

--- script/footer.js
+++ script/footer.js
@@ -3,9 +3,49 @@ class MyFooter extends HTMLElement { // source : https://stackoverflow.com/quest
     connectedCallback() {              // https://developer.mozilla.org/en-US/docs/Web/Web_Components
         //  refers back to the class object
         this.innerHTML = ` 
-        <link rel="stylesheet" href="../../css/index.css">
-        <link rel="stylesheet" href="/test.css" 
-        <div class="nav-wave">
+        <link rel="stylesheet" href="test.css" />
+        <link rel="stylesheet" href="/style/style.css"> 
+          <footer> 
+            <div class="main">
+              <div class="nav-wave-inverted"></div> 
+                <div class="contact-footer">
+                <ul>
+                    <li>Contact:</li>
+                    <li>Loremipsum@ntnu.no</li>
+                    <li>Klostergata 29 Brazil caralho</li>
+                    <li>Postnr ellerno idk</li>
+                </ul>
+                </div>
+    
+                <div class="logo">
+                <img id = "leaf" src="/GIF/leaf.gif" alt="logo">
+                </div>
+    
+                <div class="media">
+                <ul>
+                    <li>
+                    <a href="#"><img src="/image/logo/twitter.png" alt="Twitter" /></a>
+                    </li>
+                    <li>
+                    <a href="#"><img src="/image/logo/youtube.png" alt="YouTube" /></a>
+                    </li>
+                    <li>
+                    <a href="#"><img src="/image/logo/instagram.png" alt="Instagram" /></a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+          </div>
+        </footer>
+        `
+    }
+}
+
+window.customElements.define('app-footer', MyFooter) // accesses the new custom element made over and creates a new element called "app-footer"
+
+/*        <link rel="stylesheet" href="../../css/index.css">
+        <link rel="stylesheet" href="/test.css">
+            <div class="nav-wave-inverted"> 
             <footer> 
             <div class="main">
                 <div class="contact-footer">
@@ -36,9 +76,4 @@ class MyFooter extends HTMLElement { // source : https://stackoverflow.com/quest
                 </div>
             </div>
             </footer>
-    </div>
-        `
-    }
-}
-
-window.customElements.define('app-footer', MyFooter) // accesses the new custom element made over and creates a new element called "app-footer"
+    </div>*/

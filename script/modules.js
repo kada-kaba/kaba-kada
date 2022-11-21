b447278f0ffee6a4d8454414f246c0f017104579
Christio02 <112630243+Christio02@users.noreply.github.com>
2022-11-21 13:28:40 +0100

    fixed bugs

--- script/modules.js
+++ script/modules.js
@@ -1,3 +1,32 @@
+class MyNavbar extends HTMLElement { // source : https://stackoverflow.com/questions/69961205/insert-header-and-footer-on-all-webpages-using-javascript
+    connectedCallback() {              // https://developer.mozilla.org/en-US/docs/Web/Web_Components
+    //  refers back to the class object
+    this.innerHTML = ` 
+    <nav>
+        <div class="nav-wave"></div>
+        <a href="index.html" id="nav-icon">
+            <img src ="image/logo/leaf.png" alt = "green leaf logo">
+        </a>
+        <a href="about-us.html" class="link">ABOUT</a>
+        <a href="our-work.html" class="link">OUR WORK</a>
+        <a href="contact.html" class="link">CONTACT</a>
+        <div class="dropdown">
+            <button class="dropbtn">FOREST</button>
+            <div class="dropdown-content">
+                <a href="amazon.html" class="link">AMAZON</a>
+                <a href="congo.html" class="link">CONGO</a>
+                <a href="sumatra.html" class="link">SUMATRA</a>
+                <a href="xishuangbanna.html" class="link">XISHUANGBANNA</a>
+            </div>
+        </div>
+
+        <a href="donate.html" id="donate-button" class="link">DONATE</a>
+    </nav>`
+    };
+};
+
+window.customElements.define('nav-bar', MyNavbar); 
+
 // this class creates a custom "footer" element for every page on the website so that we can use modularity
 class MyFooter extends HTMLElement { // source : https://stackoverflow.com/questions/69961205/insert-header-and-footer-on-all-webpages-using-javascript
     connectedCallback() {              // https://developer.mozilla.org/en-US/docs/Web/Web_Components
@@ -39,33 +68,6 @@ class MyFooter extends HTMLElement { // source : https://stackoverflow.com/quest
     }
 }
 
-customElements.define('app-footer', MyFooter) // accesses the new custom element made over and creates a new element called "app-footer"
+window.customElements.define('app-footer', MyFooter); // accesses the new custom element made over and creates a new element called "app-footer"
 
-class MyNavbar extends HTMLElement { // source : https://stackoverflow.com/questions/69961205/insert-header-and-footer-on-all-webpages-using-javascript
-    connectedCallback() {              // https://developer.mozilla.org/en-US/docs/Web/Web_Components
-    //  refers back to the class object
-    this.innerHTML = ` 
-    <nav>
-        <div class="nav-wave"></div>
-        <a href="index.html" id="nav-icon">
-            <img src ="image/logo/leaf.png" alt = "green leaf logo">
-        </a>
-        <a href="about-us.html" class="link">ABOUT</a>
-        <a href="our-work.html" class="link">OUR WORK</a>
-        <a href="contact.html" class="link">CONTACT</a>
-        <div class="dropdown">
-            <button class="dropbtn">FOREST</button>
-            <div class="dropdown-content">
-                <a href="amazon.html" class="link">AMAZON</a>
-                <a href="congo.html" class="link">CONGO</a>
-                <a href="sumatra.html" class="link">SUMATRA</a>
-                <a href="xishuangbanna.html" class="link">XISHUANGBANNA</a>
-            </div>
-        </div>
 
-        <a href="donate.html" id="donate-button" class="link">DONATE</a>
-    </nav>`
-    }
-}
-
-customElements.define('nav-bar', MyNavbar) // accesses the new custom element made over and creates a new element called "app-footer"
